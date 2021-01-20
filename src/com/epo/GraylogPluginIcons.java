a6f4168ffb5198babfa852de3c109087a66b2586
sunsheng.zhu <sunsheng.zhu@moco.com>
2021-01-20 17:07:19 +0800

    commit

---
+++ src/com/epo/GraylogPluginIcons.java
@@ -0,0 +1,10 @@
+package com.epo;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface GraylogPluginIcons {
+    Icon IconAnAction = IconLoader.getIcon("/icons/icon.png", GraylogPluginIcons.class);
+    Icon IconToolWindow = IconLoader.getIcon("/icons/icon.png", GraylogPluginIcons.class);
+}
