8c81e94452e606cce243c5768a6ea46ba3f37c58
sunsheng.zhu <sunsheng.zhu@moco.com>
2021-01-22 17:08:36 +0800

    commit

---
+++ src/com/epo/plugin/GraylogSearchForm.java
@@ -0,0 +1,9 @@
+package com.epo.plugin;
+
+import javax.swing.*;
+
+public class GraylogSearchForm {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+}
