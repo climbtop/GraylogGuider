1309ebecf5d2404ff1eb976698ee02ce9e31b565
sunsheng.zhu <sunsheng.zhu@moco.com>
2021-01-27 12:35:23 +0800

    commit

--- src/com/epo/plugin/GraylogToolWindow.java
+++ src/com/epo/plugin/GraylogToolWindow.java
@@ -1,6 +1,9 @@
 package com.epo.plugin;
 
 import com.epo.form.GraylogSearchForm;
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
@@ -11,6 +14,7 @@ import org.jetbrains.annotations.NotNull;
 public class GraylogToolWindow implements ToolWindowFactory {
     public static String WINDOW_NAME = "Graylog";
     public static String CONTENT_NAME = "Local";
+    public static String GUIDER_NAME = "Usage";
     public static String NOTIFY_NAME = "Notify";
 
     @Override
@@ -27,6 +31,23 @@ public class GraylogToolWindow implements ToolWindowFactory {
         Content content = contentFactory.createContent(searchForm.getContentPanel(), CONTENT_NAME, false);
         //给toolWindow设置内容
         toolWindow.getContentManager().addContent(content);
+
+        //添加Guider指引说明页
+        createGuiderUsageContent(project, toolWindow);
+    }
+
+
+    private void createGuiderUsageContent(@NotNull Project project, @NotNull ToolWindow toolWindow){
+        ConsoleView consoleView = TextConsoleBuilderFactory.getInstance().createBuilder(project).getConsole();
+        Content content = toolWindow.getContentManager().getFactory().createContent(consoleView.getComponent(), GUIDER_NAME, true);
+        toolWindow.getContentManager().addContent(content);
+        StringBuffer sb = new StringBuffer();
+        sb.append("GraylogGuider Usages:\n");
+        sb.append("1. Open a target file.\n");
+        sb.append("2. Expand all code's lines in the file, that can match the right line number.\n");
+        sb.append("3. Press the Hot Key [Ctrl + Atl + G]. Disable it just press again.\n");
+        sb.append("4. Click a log output line in the file. The target's logs will be searching for.\n");
+        consoleView.print(sb.toString(), ConsoleViewContentType.NORMAL_OUTPUT);
     }
 
 }
