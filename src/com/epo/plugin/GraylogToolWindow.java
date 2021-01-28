d5dd76bf50698f0d3899c74c39a57775e22e62fd
sunsheng.zhu <sunsheng.zhu@moco.com>
2021-01-28 09:21:51 +0800

    commit

--- src/com/epo/plugin/GraylogToolWindow.java
+++ src/com/epo/plugin/GraylogToolWindow.java
@@ -19,9 +19,11 @@ public class GraylogToolWindow implements ToolWindowFactory {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        //添加Guider指引说明页
+        createGuiderUsageContent(project, toolWindow);
+
         //创建出GraylogSearchForm对象
         GraylogSearchForm searchForm = new GraylogSearchForm(project, toolWindow);
-
         //将其保存在工程
         GraylogGuiderService.getInstance().setSearchForm(searchForm);
 
@@ -31,9 +33,6 @@ public class GraylogToolWindow implements ToolWindowFactory {
         Content content = contentFactory.createContent(searchForm.getContentPanel(), CONTENT_NAME, false);
         //给toolWindow设置内容
         toolWindow.getContentManager().addContent(content);
-
-        //添加Guider指引说明页
-        createGuiderUsageContent(project, toolWindow);
     }
 
 
@@ -42,11 +41,11 @@ public class GraylogToolWindow implements ToolWindowFactory {
         Content content = toolWindow.getContentManager().getFactory().createContent(consoleView.getComponent(), GUIDER_NAME, true);
         toolWindow.getContentManager().addContent(content);
         StringBuffer sb = new StringBuffer();
-        sb.append("GraylogGuider Usages:\n\n");
-        sb.append(" 1. Open a target file.\n");
+        sb.append("GraylogGuider Configuration:\n\n");
+        sb.append(" 1. Open a target java source file.\n");
         sb.append(" 2. Expand all code's lines in the file, that can match the right line number.\n");
-        sb.append(" 3. Press the Hot Key [Ctrl + Atl + G]. Disable it just press again.\n");
-        sb.append(" 4. Click a log output line in the file. The target's logs will be searching for.\n");
+        sb.append(" 3. Press the Hot Key [Ctrl + Atl + G] to Enabled. Disable it just press again.\n");
+        sb.append(" 4. Click a logger output line in the file. The graylog logs will be searching for.\n");
         consoleView.print(sb.toString(), ConsoleViewContentType.NORMAL_OUTPUT);
     }
 
