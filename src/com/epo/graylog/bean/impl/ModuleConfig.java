285a45fbd6c3583c26c9706f1ba67d650a1358b4
sunsheng.zhu <sunsheng.zhu@moco.com>
2021-02-22 15:37:07 +0800

    commit

--- src/com/epo/graylog/bean/impl/ModuleConfig.java
+++ src/com/epo/graylog/bean/impl/ModuleConfig.java
@@ -17,10 +17,10 @@ public class ModuleConfig {
     public static String mappingFolder(String folder){
         if(folder==null || folder.trim().length()==0)
             return null;
-        if("epo-base".equals(folder)||"epo-business".equals(folder)||"hybris".equals(folder)) {
-            return "Hybris";
-        }else if("epo-official".equals(folder)) {
+        if("epo-official".equals(folder)) {
             return "MALL";
+        }else if("epo-base".equals(folder)||"epo-business".equals(folder)) {
+            return "Hybris";
         }else if("promotion".equals(folder)) {
             return "Promotion";
         }else if("sourcing".equals(folder)) {
